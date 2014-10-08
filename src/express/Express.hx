8d08857c5001ed3ab9969cce55708e4b7131b2dc
Nobukazu Hanada <tone.nobukazu@gmail.com>
2014-10-08 18:16:08 +0900

    first commit

---
+++ src/express/Express.hx
@@ -0,0 +1,12 @@
+package express;
+
+typedef Request = Dynamic;
+typedef Response = { 
+	send : String -> Void
+}
+
+
+extern class Express{
+	public function get(path:String, callback : Request -> Response -> Void) : Void;
+	public function listen(port:Int) : Void;
+}
