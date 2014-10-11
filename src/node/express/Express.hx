dc4e08c944bbacad0e2019089e1a49bc749c2f63
大村響一 <kyos1704@oomurakyouichi-no-MacBook-Air.local>
2014-10-11 18:07:54 +0900

    add test for application

--- src/node/express/Express.hx
+++ src/node/express/Express.hx
@@ -5,7 +5,13 @@ extern class Express{
 	public function set(name:String, value:String) : Void;
 
 	@:overload(function(name:String):String{})
+	@:overload(function(name:String):Bool{})
 	public function get(name:String, callback : Request -> Response -> Void) : Void;
-
+	//get -> String だがbool値とundefineも返す
 	public function listen(port:Int) : Void;
+
+	public function enable(name:String):Void;
+	public function enabled(name:String):Bool;
+	public function disable(name:String):Void;
+	public function disabled(name:String):Bool;
 }
--- test/Main.hx
+++ test/Main.hx
@@ -30,21 +30,22 @@ class Main extends BuddySuite implements Buddy{
 
 			it("Application enable",{
 				app.enable("test");
-				app.get("test").should.be(true);
-			})
+				app.enabled("test").should.be(true);
+				//get -> String だがbool値とundefineも返す
+			});
 
 			it("Application enabled",{
 				app.enabled("test").should.be(false);
 				app.enable("test");
 				app.enabled("test").should.be(true);
-			})
+			});
 
 			it("Application disable",{
 				app.enable("test");
-				app.get("test").should.be(true);
+				app.enabled("test").should.be(true);
 				app.disable("test");
-				app.get("test").should.be(false);
-			})
+				app.enabled("test").should.be(false);
+			});
 			
 			it("Application disabled", {
 				app.disabled("test").should.be(true);
@@ -52,6 +53,66 @@ class Main extends BuddySuite implements Buddy{
 				app.disabled("test").should.be(false);
 				app.disable("test");
 				app.disabled("test").should.be(true);
+			});
+
+			it("Application use",{
+				app.use("/hoge",function(res,req){});
+				true.should.be(true);
+			});
+
+			it("Application engine",{
+				app.engine("jade",equire("jade".__express));
+				true.should.be(true);
+			});
+
+			it("Application param",{
+				app.param('id',function(req,res,next,id){
+					console.log("hoge");
+					next();
+				});
+				true.should.be(true);
+			});
+
+			it("Application all",{
+				app.all('*',loadUser);
+				true.should.be(true);
+			});
+
+			it("Application route",{
+				app.route('event');
+				true.should.be(true);
+			});
+
+			it("Application locals",{
+				app.locals;
+				true.should.be(true);
+			});
+			it("Application render",{
+				app.render("email",function(err,html){
+				});
+				true.should.be(true);
+			});
+
+			it("Application listen",{
+				app.listen(3000);
+				true.should.be(true);
+			});
+
+			it("Application path",{
+				app.path("/use");
+				true.should.be(true);
+			});
+
+			it("Application mountpath",{
+				app.mountpath;
+				true.should.be(true);
+			});
+
+			it("Application onmount",{
+				app.on("mount",function(parent){
+					Console.log("admin mount");
+				});
+				true.should.be(true);
 			})
 		});
 	}
